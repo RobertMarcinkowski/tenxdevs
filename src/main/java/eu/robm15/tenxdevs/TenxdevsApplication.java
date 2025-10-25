d96e0c24c2d6aa9e0cb9c94b700be21e2582bcec
robert.marcinkowski <robert.marcinkowski@sollers.eu>
2025-10-25 18:40:52 +0200

    initial commit

---
+++ src/main/java/eu/robm15/tenxdevs/TenxdevsApplication.java
@@ -0,0 +1,13 @@
+package eu.robm15.tenxdevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenxdevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenxdevsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/robm15/tenxdevs/TenxdevsApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.robm15.tenxdevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenxdevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
