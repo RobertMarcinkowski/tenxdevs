52d86647372afe42ad4891f221a0e8bd9605b70c
robert.marcinkowski <robert.marcinkowski@sollers.eu>
2025-11-02 15:48:00 +0100

    Changes in cicd - releases - test pr and develop merge and remove feature branch.

--- src/test/java/eu/robm15/tenxdevs/controller/TenxdevsControllerTest.java
+++ src/test/java/eu/robm15/tenxdevs/controller/TenxdevsControllerTest.java
@@ -19,12 +19,12 @@ public class TenxdevsControllerTest {
 
     @Test
     void tenxdevsDefault() throws Exception {
-        mockMvc.perform(get("/tenxdevs")).andExpect(status().isOk()).andExpect(content().string("Docker test 4. Hello World!"));
+        mockMvc.perform(get("/tenxdevs")).andExpect(status().isOk()).andExpect(content().string("Docker test 5. Hello World!"));
     }
 
     @Test
     void tenxdevsWithProperty() throws Exception {
-        mockMvc.perform(get("/tenxdevs").param("name", "Robert")).andExpect(status().isOk()).andExpect(content().string("Docker test 4. Hello Robert!"));
+        mockMvc.perform(get("/tenxdevs").param("name", "Robert")).andExpect(status().isOk()).andExpect(content().string("Docker test 5. Hello Robert!"));
     }
 
 }
